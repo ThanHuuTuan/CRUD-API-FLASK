8426d57c121729ee842e43fc48415a20f409ffed
d3ni <you@example.com>
2019-10-08 20:52:53 +0200

    Sets env vars too only for this app tho

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,14 @@
 import sys
+import os 
 from students2.__init__ import create_app
 
 if __name__=='__main__':
     try:
+        os.environ['FLASK_APP'] = 'students2'
+        os.environ['FLASK_ENV'] = 'dev'
         port=sys.argv[1]
-    except:
+    except Exception:
         print("Usage: python3.7 wsgi.py <port number>")
+        sys.exit(0)
     app=create_app()
     app.run(port=port)
